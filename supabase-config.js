3b56de48351bf3215966b1e92fc5b7c52763ec12
domingosferrazfonseca283-blip <domingosferrazfonseca283@gmail.com>
2026-09-09 11:21:13 +0100

    Adicionar configuração do Supabase Storage

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.116.0';
+
+const SUPABASE_URL = 'https://bnrypbkenfvzvugilrfi.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'Sb_publishable_or5jef6ejjtVBfwDvHZ3lw_FY4uq1_i';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+export const SUPABASE_BUCKET = 'milomercios';
+export const SUPABASE_IMAGES_BUCKET = 'milomercios-imagens';
